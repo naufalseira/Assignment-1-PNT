2e2a0f5882c0dcd78cd1c221cc348cbf1c4c4784
naufalseira <kingnaufal3@gmail.com>
2024-10-26 11:27:46 +0700

    Update main.go

--- main.go
+++ main.go
@@ -2,34 +2,45 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
 
-// Handler buat (/) endpoint
-func homeHandler(w http.ResponseWriter, r * http.Request) {
-	fmt.Fprintf(w, "Welcome to the home page!")
+// Middleware untuk mencetak log request
+func loggingMiddleware(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        start := time.Now()
+        next.ServeHTTP(w, r)
+        log.Printf("%s %s %s", r.Method, r.RequestURI, time.Since(start))
+    })
 }
 
-// Handler buat (/jakarta) endpoint
+// Handler untuk (/jakarta) endpoint
 func jakartaHandler(w http.ResponseWriter, r * http.Request) {
 	fmt.Fprintf(w, "Welcome to the jakarta page!")
 }
 
-// Handler buat (/bali) endpoint
+// Handler untuk (/bali) endpoint
 func baliHandler(w http.ResponseWriter, r * http.Request) {
 	fmt.Fprintf(w, "Welcome to the bali page!")
 }
 
-// Handler buat (/papua) endpoint
+// Handler untuk (/papua) endpoint
 func papuaHandler(w http.ResponseWriter, r * http.Request) {
 	fmt.Fprintf(w, "Welcome to the papua page!")
 }
 
 func main(){
 	r := mux.NewRouter()
-	r.HandleFunc("/", homeHandler).Methods("GET")
+
+	r.Use(loggingMiddleware)
+
+	fileServer := http.FileServer(http.Dir("./static"))
+
+	r.Handle("/", fileServer).Methods("GET")
 	r.HandleFunc("/jakarta", jakartaHandler).Methods("GET")
 	r.HandleFunc("/bali", baliHandler).Methods("GET")
 	r.HandleFunc("/papua", papuaHandler).Methods("GET")
@@ -40,7 +51,11 @@ func main(){
 	fmt.Println("Endpoint 1 -> http://localhost:8090/jakarta")
 	fmt.Println("Endpoint 2 -> http://localhost:8090/bali")
 	fmt.Println("Endpoint 3 -> http://localhost:8090/papua")
-	fmt.Print("> ")
+	fmt.Println("-------------------------------------------")
+	fmt.Println("> Log request")
+	fmt.Print("")
 
-	http.ListenAndServe(":8090", r)
+	if err := http.ListenAndServe(":8090", r); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
 }
